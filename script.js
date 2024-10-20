72eb14db3b8b4c56312b5d72ce92d072916d6239
niyasbinnazeer <154691728+niyasbinnazeer@users.noreply.github.com>
2024-10-20 20:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth Scrolling for navigation links
+// Smooth scrolling for navigation links
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
